8f7b7b784da15b3d3a46eddaf592e54fdb8a5d43
Sumit <sumitkumar280601@gmail.com>
2024-09-15 23:28:56 +0530

    fix one issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import './App.css'
 import Cover from './assets/music_cover.jpg';
-import Source from './assets/follow_god_by_Kanye_West.mp3';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import ImportPlaylist from './components/ImportPlaylist';
